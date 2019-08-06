994e4ce2c62013d6fa7e35e23ef665841d807c24
Paola Latino <paolalat188@gmail.com>
2019-08-06 14:17:00 -0500

    ResultadoFinal

---
+++ Semana4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+//Estructuras
+struct Automovil{
+    var color : String
+    var llantas: intmax_t
+}
+
+var auto = Automovil(color: "Rojo", llantas: 15)
